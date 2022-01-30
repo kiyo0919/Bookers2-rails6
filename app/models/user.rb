b5bc0117e08b16d37f09fe0703ae1ac58c2c2366
kiyo0919 <kakinoki0919@gmail.com>
2022-01-30 17:47:40 +0900

    画像railsリサイズ：元画像がスクエアなら引数通りになる

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,20 +29,20 @@ class User < ApplicationRecord
   #   end
   # end
 
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize: "100x100").processed
-  end
-
-
-  # def get_profile_image(size)
+  # def get_profile_image
   #   unless profile_image.attached?
-  #     file_path = Rails.root.join("app/assets/images/no_image.jpg")
-  #     profile_image.attach(io: File.open(file_path),filename: "default-image.jpg",content_type: "image/jpeg")
+  #     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+  #     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   #   end
-  #   profile_image.variant(resize: size).processed
+  #   profile_image.variant(resize: "100x100").processed
   # end
+
+
+  def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      profile_image.attach(io: File.open(file_path),filename: "default-image.jpg",content_type: "image/jpeg")
+    end
+    profile_image.variant(resize: size).processed
+  end
 end
