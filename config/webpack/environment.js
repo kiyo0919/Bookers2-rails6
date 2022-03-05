0d367ec01d65aafb168de3d82dea8f467f25560b
ls-contents <learningsuccess@infratop.jp>
2022-03-05 12:05:14 +0900

    コメントアウト追記

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,8 +6,8 @@ const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
+    $: 'jquery/src/jquery',       // ここの記述がカルキュラムと違う場合、非同期が行われないことがある。bootstrap導入の章で説明あり。
+    jQuery: 'jquery/src/jquery',   //エラーとしては検証ツールに Uncaught ReferenceError: $ is not definedが出て、jqueryが定義されていないとでる。
     Popper: 'popper.js'
   })
 )
